7efe489a93b9752b5a76ad8f147571e422b98498
Miek Messerschmidt <mmesserschmiddt@gmail.com>
2023-01-20 11:06:50 +0200

    implements patter projection

--- minitt/pattern.py
+++ minitt/pattern.py
@@ -7,23 +7,24 @@ from .helpers import Name
 
 if TYPE_CHECKING:
     from .values import Value
-    
+
+
 class Pattern(Protocol):
     def __contains__(self, name: Name) -> bool:
         ...
-        
-    def project(self, name:Name, in_:"Value")->"Value":
-        ...    
 
-    
+    def project(self, name: Name, in_: "Value") -> "Value":
+        ...
+
 
 @dataclass
 class EmptyPattern(Pattern):
     def __contains__(self, name: Name) -> bool:
         return False
 
-    def project(self, name:Name, in_:"Value")->"Value":
+    def project(self, name: Name, in_: "Value") -> "Value":
         raise Critical(f"{self} is empty. Does not contain {name}")
+
 
 @dataclass
 class PairPattern(Pattern):
@@ -32,9 +33,17 @@ class PairPattern(Pattern):
 
     def __contains__(self, name: Name) -> bool:
         return (name in self.a) or (name in self.b)
-    
-    def project(self, name:Name, in_:"Value")->"Value":
-        raise NotImplementedError
+
+    def project(self, name: Name, in_: "Value") -> "Value":
+        from .evaluate import first, second
+
+        if name in self.a:
+            return self.a.project(name, first(in_))
+        elif name in self.b:
+            return self.b.project(name, second(in_))
+        else:
+            raise Critical("projection error")
+
 
 @dataclass
 class VariablePattern(Pattern):
@@ -43,6 +52,8 @@ class VariablePattern(Pattern):
     def __contains__(self, name: Name):
         return name == self.name
 
-
-    def project(self, name:Name, in_:"Value")->"Value":
-        raise NotImplementedError
+    def project(self, name: Name, in_: "Value") -> "Value":
+        if self.name == name:
+            return in_
+        else:
+            raise Critical("projection error")
